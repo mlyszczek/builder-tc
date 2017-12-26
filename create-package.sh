0f9868beb62e7a69aa1b93c5402de81996208d6e
Michał Łyszczek <michal.lyszczek@bofc.pl>
2017-12-26 16:30:57 +0100

    Added arch armv7a9, refactored scripts for new arch

--- create-package.sh
+++ create-package.sh
@@ -8,28 +8,27 @@ create_native()
     chmod a+rX -R tmp/usr/native-toolchain
     cp native-control.template tmp/CONTROL/control
     sed -i "s/\${ARCH}/${arch}/g" tmp/CONTROL/control
-    sed -i "s/\${TARGET}/${CT_TARGET}/" tmp/CONTROL/control
+    sed -i "s/\${TARGET}/${ct_target}/" tmp/CONTROL/control
 }
 
 
 create_cross()
 {
-    path=`echo "${tc_path}" | sed "s/\\${CT_TARGET}/${CT_TARGET}/g"`
+    path=`echo "${tc_path}" | sed "s/\\${CT_TARGET}/${ct_target}/g"`
     mkdir tmp/usr/cross-toolchain
     cp -r ${path} tmp/usr/cross-toolchain
     chmod a+rX -R tmp/usr/cross-toolchain
     cp cross-control.template tmp/CONTROL/control
-    sed -i "s/\${TARGET}/${CT_TARGET}/" tmp/CONTROL/control
+    sed -i "s/\${TARGET}/${ct_target}/" tmp/CONTROL/control
 }
 
 
-arch=`readlink .config | cut -f2 -d-`
-lib="gnu"
 vendor=`grep CT_TARGET_VENDOR=\" .config | cut -f2 -d\"`
 kernel=`grep CT_KERNEL=\" .config | cut -f2 -d\"`
 tc_path=`grep CT_PREFIX_DIR=\" .config | cut -f2 -d\"`
 tc_type=`grep CT_TOOLCHAIN_TYPE=\" .config | cut -f2 -d\"`
-CT_TARGET="${arch}-${vendor}-${kernel}-${lib}"
+ct_target=`readlink .config | cut -f3 -d.`
+arch=`echo ${ct_target} | cut -f1 -d-`
 
 
 rm -rf tmp
