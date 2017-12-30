cd88d7ba693ee6b2e0e198a020f8ef7a45cb14bf
Michał Łyszczek <michal.lyszczek@bofc.pl>
2017-12-30 20:29:16 +0100

    added x86 uclib toolchain

--- fix-missing-libintl-in-uclibc.sh
+++ fix-missing-libintl-in-uclibc.sh
@@ -2,7 +2,16 @@
 
 target=`readlink .config | cut -f3 -d.`
 tc_path=`grep CT_PREFIX_DIR=\" .config | cut -f2 -d\" | cut -f1-3 -d/`
+tc_type=`grep CT_TOOLCHAIN_TYPE=\" .config | cut -f2 -d\"
 build_path=".build"
+
+if [ ${tc_type} = "canadian" ]
+then
+    ###
+    # only cross toolchains need fixing
+    #
+    exit 0
+fi
 
 if [[ "${target}" = *"-uclibc" ]]
 then
